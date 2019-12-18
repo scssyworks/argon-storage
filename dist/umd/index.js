d09ed84539028a5bad1acd7bf8066c4cfb2551e3
Sachin Singh <ssingh259@sapient.com>
2019-12-18 15:33:13 +0530

    Updated packagesa

--- dist/umd/index.js
+++ dist/umd/index.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = global || self, factory(global.ArgonStorage = {}));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
   function _typeof(obj) {
     if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
@@ -516,5 +516,5 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
+})));
 //# sourceMappingURL=index.js.map
